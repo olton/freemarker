cb896ed7daa5e33752e56172f81b067fb3e4c666
Serhii Pimenov <serhii@pimenov.com.ua>
2025-11-29 14:45:17 +0200

    remove fmpp source

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import { Freemarker } from './main/freemarker.js'
-import { info } from './util/info.js'
 
-export { info }
 export default Freemarker
--- src/util/info.js
+++
@@ -1,8 +0,0 @@
-const version = '__VERSION__'
-const build_time = '__BUILD_TIME__'
-
-const info = () => {
-    console.info(`Freemarker v${version}, built ${build_time}`)
-}
-
-export { info }
